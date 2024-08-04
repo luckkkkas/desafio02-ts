e1f7d0961a8ad377775eb7b3cda61b91bbb9f8d1
luckkkkas <lucashbo321@gmail.com>
2024-08-04 12:37:48 -0300

    Header Complete

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,8 +5,11 @@ import { ChakraProvider,
   Avatar,
   MenuButton,
   MenuList,
-  MenuItem
+  MenuItem,
+  Menu,
+  Heading
  } from "@chakra-ui/react"
+ import { ChevronDownIcon } from "@chakra-ui/icons"
 
 export const Header  = () => {
   return(
@@ -15,9 +18,18 @@ export const Header  = () => {
         <Flex pl={1.5} backgroundColor='black' w='full' h={75}>
           <Image boxSize={90} mt={0} src='https://hermes.digitalinnovation.one/assets/diome/logo-full.svg' alt='Dan Abramov' />
           <Spacer />
-           
-              <Avatar size={'7xs'} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGfHP4p65dQ0MnpidZ1GIJM5jqDgLJBLdWRA&s'  />
-           
+          <Heading as={'h1'} color={'whatsapp.100'} m={'auto'}>DIO Banking</Heading>
+          <Spacer />
+           <Menu>
+              <MenuButton m={'auto'} mr={10} boxSize={10} as={Avatar} rightIcon={<ChevronDownIcon />}>
+                  <MenuList>
+                    <MenuItem>Account</MenuItem>
+                    <MenuItem>Change user</MenuItem>
+                    <MenuItem>Settings</MenuItem>
+                    <MenuItem>Exit</MenuItem>
+                  </MenuList>
+              </MenuButton>
+            </Menu>
         </Flex>
         
          
